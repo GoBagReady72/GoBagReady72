5b8bd4d6f3e2e1e7ea1261f0ff7a4c7249ed69fd
GoBagReady72 <gobagready72@gmail.com>
2025-10-15 16:10:43 -0400

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,4 @@
 // ESM because "type":"module" in package.json
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, app: 'game', time: new Date().toISOString() });
+  res.status(200).json({ ok: true, app: 'game', time: new Date().toISOString() }); //bump
 }
