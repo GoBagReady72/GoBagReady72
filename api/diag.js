67c47236840ced2fdf3950d12ef93ae1bc64f80b
GoBagReady72 <gobagready72@gmail.com>
2025-10-15 18:35:07 -0400

    Delete api/diag.js

--- api/diag.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    method: req.method,
-    has_ADMIN_TRACK_ENDPOINT: !!process.env.ADMIN_TRACK_ENDPOINT,
-    has_ADMIN_SHARED_SECRET: !!process.env.ADMIN_SHARED_SECRET,
-    forwarded_origin: process.env.FORWARDED_ORIGIN || null,
-    node: process.versions?.node || null
-  });
-}
